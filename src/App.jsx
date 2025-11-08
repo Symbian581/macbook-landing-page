7749060f3945979e23a0392b3a30a512e07bfc6a
Symbian581 <incsymbian@gmail.com>
2025-11-08 11:11:59 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <main className='bg-black h-screen'>
+        <NavBar />
+    </main>
+  )
+}
+
+export default App
